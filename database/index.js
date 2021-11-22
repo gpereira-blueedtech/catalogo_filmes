21a05a72aadb30808e2405f82a61b5731ec95532
Gabriel Lima <gpereira@blueedtech.com.br>
2021-11-22 19:29:34 -0300

    Alterado para DB Heroku

--- database/index.js
+++ database/index.js
@@ -1,23 +1,23 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-  host:  process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres'
-});
+// const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
+//   host:  process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: 'postgres'
+// });
 
 
 // Conectando com o DB do Heroku
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {     
-//   dialect: 'postgres',
-//   protocol: 'postgres',     
-//   dialectOptions: {         
-//     ssl: {             
-//       require: true,             
-//       rejectUnauthorized: false         
-//     }     
-//   } 
-// }) 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {     
+  dialect: 'postgres',
+  protocol: 'postgres',     
+  dialectOptions: {         
+    ssl: {             
+      require: true,             
+      rejectUnauthorized: false         
+    }     
+  } 
+}) 
 
 module.exports = sequelize;
